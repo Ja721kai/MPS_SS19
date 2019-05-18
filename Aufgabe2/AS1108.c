c4ca4149765c2c3f73883cd7a74f998646f2b180
Jan Kaiser <jan.kaiser@htwg-konstanz.de>
2019-05-19 00:05:17 +0200

    Program running, state machine needs to be optimized

--- Aufgabe2/AS1108.c
+++ Aufgabe2/AS1108.c
@@ -17,58 +17,76 @@
 // es sind geeignete Datenstrukturen für den Datenaustausch
 // zwischen den Handlern festzulegen.
 
-LOCAL Void AS1108_Write(UChar adr, UChar arg) {
-   Char ch = UCA1RXBUF;   // dummy read, UCRXIFG := 0, UCOE := 0
-   CLRBIT(P2OUT,  BIT3);  // Select aktivieren
-   UCA1TXBUF = adr;       // Adresse ausgeben
-   while (TSTBIT(UCA1IFG, UCRXIFG) EQ 0);
-   ch = UCA1RXBUF;        // dummy read
-   UCA1TXBUF = arg;       // Datum ausgeben
-   while (TSTBIT(UCA1IFG, UCRXIFG) EQ 0);
-   ch = UCA1RXBUF;        // dummy read
-   SETBIT(P2OUT,  BIT3);  // Select deaktivieren
+typedef enum
+{
+    S0, S1, S2, S3, S4, S5
+} SegState;
+LOCAL int digits[4] = {4, 3, 2, 1};
+LOCAL int selectedValue = 0;
+LOCAL int currentValue = 0;
+LOCAL int displayedValue[4] = {0, 0, 0, 0};
+GLOBAL Bool mode = 1;  // mode 1 -> increment, mode 0 -> decrement
+
+LOCAL Void AS1108_Write(UChar adr, UChar arg)
+{
+    Char ch = UCA1RXBUF;   // dummy read, UCRXIFG := 0, UCOE := 0
+    CLRBIT(P2OUT, BIT3);  // Select aktivieren
+    UCA1TXBUF = adr;       // Adresse ausgeben
+    while (TSTBIT(UCA1IFG, UCRXIFG) EQ 0)
+        ;
+    ch = UCA1RXBUF;        // dummy read
+    UCA1TXBUF = arg;       // Datum ausgeben
+    while (TSTBIT(UCA1IFG, UCRXIFG) EQ 0)
+        ;
+    ch = UCA1RXBUF;        // dummy read
+    SETBIT(P2OUT, BIT3);  // Select deaktivieren
 }
 
+GLOBAL Void SPI_Init(Void)
+{
+    // set up Pin 3 at Port 2 => CS output, High
+    SETBIT(P2OUT, BIT3);
+    SETBIT(P2DIR, BIT3);
+    CLRBIT(P2SEL0, BIT3);
+    CLRBIT(P2SEL1, BIT3);
+    CLRBIT(P2REN, BIT3);
 
-GLOBAL Void SPI_Init(Void) {
-   // set up Pin 3 at Port 2 => CS output, High
-   SETBIT(P2OUT,  BIT3);
-   SETBIT(P2DIR,  BIT3);
-   CLRBIT(P2SEL0, BIT3);
-   CLRBIT(P2SEL1, BIT3);
-   CLRBIT(P2REN,  BIT3);
+    // set up Pins 4, 5 and 6 at Port 2
+    CLRBIT(P2SEL0, BIT4 + BIT5 + BIT6);
+    SETBIT(P2SEL1, BIT4 + BIT5 + BIT6);
 
-   // set up Pins 4, 5 and 6 at Port 2
-   CLRBIT(P2SEL0, BIT4 + BIT5 + BIT6);
-   SETBIT(P2SEL1, BIT4 + BIT5 + BIT6);
+    // set up Universal Serial Communication Interface A
+    SETBIT(UCA1CTLW0, UCSWRST);        // UCA1 software reset
 
-   // set up Universal Serial Communication Interface A
-   SETBIT(UCA1CTLW0, UCSWRST);        // UCA1 software reset
+    // in Übereinstimung mit dem SPI-Timing-Diagramm von AS1108
+    UCA1CTLW0_H = 0b10101001;          // clock phase: rising edge, ...
+                                       // ... clock polarity: inactive low ...
+                                       // ... MSB first, 8-bit data, SPI master mode, ...
+                                       // ... 3-pin SPI, synchronous mode
 
-   // in Übereinstimung mit dem SPI-Timing-Diagramm von AS1108
-   UCA1CTLW0_H = 0b10101001;          // clock phase: rising edge, ...
-                                      // ... clock polarity: inactive low ...
-                                      // ... MSB first, 8-bit data, SPI master mode, ...
-                                      // ... 3-pin SPI, synchronous mode
+    CLRBIT(UCA1CTLW0, UCSSEL1);        // select ACLK ...
+    SETBIT(UCA1CTLW0, UCSSEL0);        // .. as source clock
+    UCA1BRW = 0;                       // prescaler
 
-   CLRBIT(UCA1CTLW0, UCSSEL1);        // select ACLK ...
-   SETBIT(UCA1CTLW0, UCSSEL0);        // .. as source clock
-   UCA1BRW = 0;                       // prescaler
-
-   CLRBIT(UCA1CTLW0, UCSWRST);        // release the UCA0 for operation
+    CLRBIT(UCA1CTLW0, UCSWRST);        // release the UCA0 for operation
 }
-
 
 // der Treiberbaustein AS1108 ist hier über die SPI-Schnittstelle zu initialisieren
-GLOBAL Void AS1108_Init(Void) {
+GLOBAL Void AS1108_Init(Void)
+{
 
     AS1108_Write(0x0C, 0x01);  // reset feature register to default setting
 
-    // set all 4 digits to ZERO
-    AS1108_Write(0x01, 0x00);
-    AS1108_Write(0x02, 0x00);
+    /* set digits to "HEHE"
+    AS1108_Write(0x03, 0x0B);
+    AS1108_Write(0x04, 0x0C);
+    AS1108_Write(0x01, 0x0B);
+    AS1108_Write(0x02, 0x0C);*/
+
     AS1108_Write(0x03, 0x00);
     AS1108_Write(0x04, 0x00);
+    AS1108_Write(0x01, 0x00);
+    AS1108_Write(0x02, 0x00);
 
     AS1108_Write(0x09, 0xFF);  // decode mode, B-/HEX for all digits
 
@@ -84,21 +102,112 @@ GLOBAL Void AS1108_Init(Void) {
 // ----------------------------------------------------------------------------
 
 // der Button-Handler beinhaltet keine Zustandsmaschine
-GLOBAL Void Button_Handler(Void) {
-
+GLOBAL Void Button_Handler(Void)
+{
+    // switch modes?
+    if (tst_event(EVENT_7LED))
+    {
+        clr_event(EVENT_7LED);
+        mode ^= 1; // toggle mode bit
+    }
+    // button 6-3 pressed?
+    if (tst_event(EVENT_BTN3))
+    {
+        clr_event(EVENT_BTN3);
+        selectedValue = 1;
+        set_event(EVENT_10); // Handler 2 Event
+    }
+    if (tst_event(EVENT_BTN4))
+    {
+        clr_event(EVENT_BTN4);
+        selectedValue = BASE;
+        set_event(EVENT_10); // Handler 2 Event
+    }
+    if (tst_event(EVENT_BTN5))
+    {
+        clr_event(EVENT_BTN5);
+        selectedValue = BASE * BASE;
+        set_event(EVENT_10); // Handler 2 Event
+    }
+    if (tst_event(EVENT_BTN6))
+    {
+        clr_event(EVENT_BTN6);
+        selectedValue = BASE * BASE * BASE;
+        set_event(EVENT_10); // Handler 2 Event
+    }
 }
 
 // ----------------------------------------------------------------------------
 
 // der Number-Handler beinhaltet keine Zustandsmaschine
-GLOBAL Void Number_Handler(Void) {
+GLOBAL Void Number_Handler(Void)
+{
+    if (tst_event(EVENT_10))
+    {
+        clr_event(EVENT_10);
 
+        if(mode) {
+            currentValue += selectedValue; // TODO: Implement calculation
+        } else {
+            currentValue -= selectedValue;
+        }
+        // handle overflow:
+        if(currentValue GE 10000) {
+            currentValue -= 10000;
+        }
+        if (currentValue LT 0) {
+            currentValue += 10000;
+        }
+
+        displayedValue[3] = currentValue / 1000;
+        displayedValue[2] = (currentValue % 1000) / 100;
+        displayedValue[1] = (currentValue % 100) / 10;
+        displayedValue[0] = currentValue % 10;
+
+        set_event(EVENT_11);  // Handler 3 Event
+    }
 }
 
 // ----------------------------------------------------------------------------
 
 // der AS1108_Hander beinhaltet eine Zustandsmaschine
-GLOBAL Void AS1108_Handler(Void) {
+GLOBAL Void AS1108_Handler(Void)
+{
+    // TODO: Implement states with function pointers
+    static SegState state = S0;
+    static int digit;
+    static char ch;
+
+    switch (state)
+    {
+    case(S0):
+        if(tst_event(EVENT_11)) {
+            digit = 0x01;  // initialize with index at digit 0x04
+            state = S1;
+        }
+        break;
+    case(S1):
+        // set digit:  (implement this routine into different states later on
+        ch = UCA1RXBUF;   // dummy read, UCRXIFG := 0, UCOE := 0
+        CLRBIT(P2OUT, BIT3);  // Select aktivieren
+        UCA1TXBUF = digit;       // Adresse ausgeben
+        while (TSTBIT(UCA1IFG, UCRXIFG) EQ 0)
+            ;
+        ch = UCA1RXBUF;        // dummy read
+        UCA1TXBUF = displayedValue[digit-1];       // Datum ausgeben
+        while (TSTBIT(UCA1IFG, UCRXIFG) EQ 0)
+            ;
+        ch = UCA1RXBUF;        // dummy read
+        SETBIT(P2OUT, BIT3);  // Select deaktivieren
+        digit++;
+        if(digit EQ 0x05) {
+            clr_event(EVENT_11);
+            state = S0;
+        }
+        break;
+    default:
+            break;
+}
 
 }
 
--- Aufgabe2/TA0.c
+++ Aufgabe2/TA0.c
@@ -19,14 +19,11 @@
 static Char muster = 0;
 static Short counter = 0;
 
-
-
 const Char n = 6;
 const Char num_buttons = 2;  // Button 1 und 2
 const Char num_button_states = 2;  // State, Cnt (Folie 45)
-static Bool states[2] = {0, 1};
-static Char cnt[6] = {0, 1, 2, 3, 4, 5};
-
+static Bool states[2] = { 0, 1 };
+static Char cnt[6] = { 0, 1, 2, 3, 4, 5 };
 
 GLOBAL Void set_blink_muster(UInt arg)
 {
@@ -67,6 +64,9 @@ __interrupt Void TA0_ISR(Void)
     static Bool btn1;
     static Bool btn2;
     static Bool btn3;
+    static Bool btn4;
+    static Bool btn5;
+    static Bool btn6;
 
     static Bool* ptr_state1 = &states[0];
     static Char* ptr_cnt1 = &cnt[0];
@@ -74,10 +74,19 @@ __interrupt Void TA0_ISR(Void)
     static Char* ptr_cnt2 = &cnt[0];
     static Bool* ptr_state3 = &states[0];
     static Char* ptr_cnt3 = &cnt[0];
+    static Bool* ptr_state4 = &states[0];
+    static Char* ptr_cnt4 = &cnt[0];
+    static Bool* ptr_state5 = &states[0];
+    static Char* ptr_cnt5 = &cnt[0];
+    static Bool* ptr_state6 = &states[0];
+    static Char* ptr_cnt6 = &cnt[0];
 
     btn1 = TSTBIT(P1IN, BIT1);
     btn2 = TSTBIT(P1IN, BIT0);
-    btn3 = TSTBIT("PIN", "BIT");
+    btn3 = TSTBIT(P3IN, BIT0);
+    btn4 = TSTBIT(P3IN, BIT1);
+    btn5 = TSTBIT(P3IN, BIT2);
+    btn6 = TSTBIT(P3IN, BIT3);
 
     /*Hysterese*/
     if (btn1)
@@ -85,11 +94,12 @@ __interrupt Void TA0_ISR(Void)
         if (*ptr_cnt1 == n - 1 && *ptr_state1 == 0)
         {
             ptr_state1++;
-            if (!tst_event(EVENT_BTN1)) {
+            if (!tst_event(EVENT_BTN1))
+            {
                 set_event(EVENT_BTN1);
             }
         }
-        if (*ptr_cnt1 == 0  && *ptr_state1 == 1)
+        if (*ptr_cnt1 == 0 && *ptr_state1 == 1)
         {
             ptr_state1--;
         }
@@ -113,6 +123,7 @@ __interrupt Void TA0_ISR(Void)
         {
             ptr_state2++;
             set_event(EVENT_BTN2);
+            set_event(EVENT_7LED); // signal to switch mode in AS1108
             __low_power_mode_off_on_exit();
         }
         if (*ptr_cnt2 == 0 && *ptr_state2 == 1)
@@ -132,30 +143,101 @@ __interrupt Void TA0_ISR(Void)
         }
     }
     if (btn3)
+    {
+        if (*ptr_cnt3 == n - 1 && *ptr_state3 == 0)
         {
-            if (*ptr_cnt3 == n - 1 && *ptr_state3 == 0)
-            {
-                ptr_state3++;
-                /*set_event(EVENT_BTN3);
-                __low_power_mode_off_on_exit();*/
-                SETBIT(P1OUT, BIT2);  // Error LED for button testing
-            }
-            if (*ptr_cnt3 == 0 && *ptr_state3 == 1)
-            {
-                ptr_state3--;
-            }
-            if (*ptr_cnt3 < n - 1)
-            {
-                ptr_cnt3++;
-            }
+            ptr_state3++;
+            set_event(EVENT_BTN3);
+            __low_power_mode_off_on_exit();
         }
-        else
+        if (*ptr_cnt3 == 0 && *ptr_state3 == 1)
         {
-            if (*ptr_cnt3 > 0)
-            {
-                ptr_cnt3--;
-            }
+            ptr_state3--;
         }
+        if (*ptr_cnt3 < n - 1)
+        {
+            ptr_cnt3++;
+        }
+    }
+    else
+    {
+        if (*ptr_cnt3 > 0)
+        {
+            ptr_cnt3--;
+        }
+    }
+    if (btn4)
+    {
+        if (*ptr_cnt4 == n - 1 && *ptr_state4 == 0)
+        {
+            ptr_state4++;
+            set_event(EVENT_BTN4);
+            __low_power_mode_off_on_exit();
+        }
+        if (*ptr_cnt4 == 0 && *ptr_state4 == 1)
+        {
+            ptr_state4--;
+        }
+        if (*ptr_cnt4 < n - 1)
+        {
+            ptr_cnt4++;
+        }
+    }
+    else
+    {
+        if (*ptr_cnt4 > 0)
+        {
+            ptr_cnt4--;
+        }
+    }
+    if (btn5)
+    {
+        if (*ptr_cnt5 == n - 1 && *ptr_state5 == 0)
+        {
+            ptr_state5++;
+            set_event(EVENT_BTN5);
+            __low_power_mode_off_on_exit();
+        }
+        if (*ptr_cnt5 == 0 && *ptr_state5 == 1)
+        {
+            ptr_state5--;
+        }
+        if (*ptr_cnt5 < n - 1)
+        {
+            ptr_cnt5++;
+        }
+    }
+    else
+    {
+        if (*ptr_cnt5 > 0)
+        {
+            ptr_cnt5--;
+        }
+    }
+    if (btn6)
+    {
+        if (*ptr_cnt6 == n - 1 && *ptr_state6 == 0)
+        {
+            ptr_state6++;
+            set_event(EVENT_BTN6);
+            __low_power_mode_off_on_exit();
+        }
+        if (*ptr_cnt6 == 0 && *ptr_state6 == 1)
+        {
+            ptr_state6--;
+        }
+        if (*ptr_cnt6 < n - 1)
+        {
+            ptr_cnt6++;
+        }
+    }
+    else
+    {
+        if (*ptr_cnt6 > 0)
+        {
+            ptr_cnt6--;
+        }
+    }
 
     switch (muster)
     {
